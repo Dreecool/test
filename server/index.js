1e23d148523f72acde0eb4f7d1170e7278038d53
Dreecool <114856837+Dreecool@users.noreply.github.com>
2023-09-11 22:06:51 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const Register = require("./model/register");
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://test-pvg7.vercel.app"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   })
