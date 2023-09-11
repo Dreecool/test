519eee0540459c06cfe20a9288f8729ebbcf519b
Dreecool <114856837+Dreecool@users.noreply.github.com>
2023-09-11 22:37:55 +0800

    Update privateRoute.js

--- client/src/privateRoutes/privateRoute.js
+++ client/src/privateRoutes/privateRoute.js
@@ -1,47 +1,29 @@
-import { useEffect, useRef, useState } from 'react';
-import { Outlet, Navigate } from 'react-router-dom';
-import Axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Outlet, Navigate } from "react-router-dom";
+import Axios from "axios";
 
- Axios.defaults.withCredentials = true
+Axios.defaults.withCredentials = true;
 
 const PrivateRoutes = () => {
-
- 
-
   const [isAuthenticated, setIsAuthenticated] = useState(true);
 
-  const data = useRef()
-  
-
   useEffect(() => {
+    Axios.get("https://test-wine-five-20.vercel.app/LoggedIn")
+      .then((response) => {
+        console.log(response);
 
-    Axios.get("https://test-wine-five-20.vercel.app/LoggedIn").then((response) => {
+        if (response.data.Message === "Authorized") {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    console.log(response)
-      
-    data.current = response.data.Message
-
-    if(data.current === "Authorized") {
-
-      setIsAuthenticated(true)
-
-    } else {
-
-      setIsAuthenticated(false)
-
-    }
-   
-    })
-
-  
-  })
-
-  
-
-
-  
-  return isAuthenticated ? <Outlet /> : <Navigate to='/' />;
-
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
 };
 
-export default PrivateRoutes
+export default PrivateRoutes;
